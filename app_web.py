e96a5017a26415e649c7ab4a06b8188ac7568ab1
alisonadriano <alisododo@gmail.com>
2026-08-07 16:53:08 -0400

    atualizando o sistema

--- app_web.py
+++ app_web.py
@@ -529,7 +529,7 @@ if st.session_state["is_admin"]:
 
 # Agora o Streamlit desenha a barra lateral na ordem correta
 with st.sidebar:
-    st.image("assets/logo.png", use_container_width=True)
+    st.image("assets/logo.png", use_container_width=130)
     st.title("💰 FinanceiroPro")
     st.write(f"👤 Usuário ID: **{st.session_state['usuario_id']}** {'(👑 Admin)' if st.session_state['is_admin'] else ''}")
     opcao = st.selectbox("Menu de Navegação", opcoes_menu)
