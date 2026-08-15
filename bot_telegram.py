82b8444b0dd9d59adcbf6211f2a2fa084303315a
alisonadriano <alisododo@gmail.com>
2026-08-15 10:34:37 -0400

    atualizando o sistema

--- bot_telegram.py
+++ bot_telegram.py
@@ -9,7 +9,7 @@ from dateutil.relativedelta import relativedelta
 
 from dotenv import load_dotenv
 import pytz
-from supabase import Client, create_client
+from supabase import Client, create_client, ClientOptions
 from telegram import (
     InlineKeyboardButton,
     InlineKeyboardMarkup,
@@ -36,7 +36,12 @@ SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
 STREAMLIT_URL = "https://financeiro-web-2-0.streamlit.app/?uid=1"
 
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+options = ClientOptions(
+    postgrest_client_timeout=30,
+    storage_client_timeout=30
+)
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY, options=options)
 logging.basicConfig(level=logging.INFO)
 
 CACHE_USUARIOS = {}
